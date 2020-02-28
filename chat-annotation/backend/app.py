bbd8d0ff2cf0c4f507f106b81384d35e978a8a58
bowbowbow <clsrn1581@gmail.com>
2020-02-29 01:56:54 +0900

    add get scenario api

--- chat-annotation/backend/app.py
+++ chat-annotation/backend/app.py
@@ -1,6 +1,8 @@
-import os, sys, datetime, random
-from flask import Flask, session, g, request, render_template, redirect
+import os, sys, datetime, random, json
+from flask import Flask, Response, session, g, request, render_template, redirect
 from flask_mongoengine import MongoEngine
+
+from models import Scenario, Annotation
 
 base_dir = os.path.abspath(os.path.dirname(__file__) + '/')
 sys.path.append(base_dir)
@@ -15,9 +17,16 @@ db = MongoEngine(app)
 def index():
     return render_template('index.html')
 
-@app.route("/", methods=["GET"])
-def index():
-    return render_template('index.html')
+
+@app.route("/api/scenario", methods=["GET"])
+def get_scenario():
+    # /api/scenario?key=gzkupvhjzo
+    random_string_key = request.args.get('key', '')
+    try:
+        scenario = Scenario.objects.get(random_string_key=random_string_key)
+        return json.dumps({'scenario': scenario.dump()})
+    except Scenario.DoesNotExist:
+        return Response(status=404)
 
 
 if __name__ == '__main__':
