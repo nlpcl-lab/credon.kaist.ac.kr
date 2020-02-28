ea5bcda861f72a6708af99f60b164793c9b0f62c
bowbowbow <clsrn1581@gmail.com>
2020-02-29 01:15:59 +0900

    update insert script

---
+++ chat-annotation/backend/dbscript.py
@@ -0,0 +1,64 @@
+import os
+import json
+import argparse
+from tqdm import tqdm
+import pickle
+from bson import json_util
+from mongoengine import connect
+
+from models import Scenario, Annotation
+import config as config
+from datetime import datetime
+
+collections = ['scenario', 'annotation']
+
+
+def insert_data(path):
+    with open(path) as f:
+        data = json.load(f)
+        for item in tqdm(data):
+            doc_id = item['doc_id']
+            chat_scenario = item['chat_scenario']
+
+            if Scenario.objects.filter(doc_id=doc_id).count():
+                continue
+
+            Scenario(doc_id=doc_id, chat_scenario=chat_scenario).save()
+
+
+def db_backup(memo):
+    import datetime
+
+    backup_dir = './backup/' + datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+    os.makedirs(backup_dir)
+
+    memo_path = os.path.abspath(backup_dir + '/memo.txt')
+    with open(memo_path, 'w') as f:
+        f.write(memo)
+
+    import subprocess
+
+    MONGODB_SETTINGS = config.Config.MONGODB_SETTINGS
+    for collection in collections:
+        print('now backup collection {}'.format(collection))
+        backup_path = os.path.abspath(backup_dir + '/{}'.format(collection))
+        subprocess.call(['mongoexport',
+                         '-h',
+                         '{}:{}'.format(MONGODB_SETTINGS['host'], MONGODB_SETTINGS['port']),
+                         '-u',
+                         MONGODB_SETTINGS['username'],
+                         '-p',
+                         MONGODB_SETTINGS['password'],
+                         '-d',
+                         MONGODB_SETTINGS['db'],
+                         '-c',
+                         collection,
+                         '-o',
+                         backup_path,
+                         ])
+
+
+if __name__ == '__main__':
+    connect(**config.Config.MONGODB_SETTINGS)
+
+    insert_data('./data/chat-v-0.2.json')
